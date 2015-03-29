1d0cbe2076f597d730cc2a20cd2c119db7045928
345ishaan <345ishaan@gmail.com>
2015-03-30 00:21:05 +0530

    Update settings.py

--- gmail_api/settings.py
+++ gmail_api/settings.py
@@ -1,8 +1,8 @@
 import os
 application_handler_setttings = dict(
-            google_consumer_key="536344957637-0jpnbu58j1qf5ev40gn6c2a90118oh81.apps.googleusercontent.com",
-			google_consumer_secret="UpEwFrI30KLah1gfsIabsOen",
-            cookie_secret= '5d856a13-775d-4efb-94f0-828e6d000567',
+            google_consumer_key="",
+	    google_consumer_secret="",
+            cookie_secret= '',
             debug=True,
-			google_oauth =  dict(key= '536344957637-0jpnbu58j1qf5ev40gn6c2a90118oh81.apps.googleusercontent.com', secret= 'UpEwFrI30KLah1gfsIabsOen')
+	    google_oauth =  dict(key= '', secret= '')
             )
